ba5084145e9e39d5f2d6aff151a0555e6cffbe0a
Shivam <main.shivamsharma@gmail.com>
2025-06-14 11:55:55 +0530

    frontend push

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const path = require("path");
 
 router.post("/chat", rootController);
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "..", "frontend", "chat.html"));
+});
 
 module.exports = router;
